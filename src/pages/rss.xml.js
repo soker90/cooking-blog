12650db1e272bf6975aae58b0478267ce9fd692b
Eduardo Parra <eduparra90@gmail.com>
2024-03-21 00:48:38 +0100

    fix custom rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,13 @@
-import rss from '@astrojs/rss';
-import { SITE } from '../config'
+import rss from "@astrojs/rss";
+import { SITE } from "../config";
 
-const allPosts = import.meta.glob('./blog/*.md', { eager: true })
-const sortedPosts = Object.values(allPosts).sort((a, b) => new Date(b.date) - new Date(a.date));
+const allPosts = import.meta.glob("./blog/*.md", { eager: true });
+const sortedPosts = Object.values(allPosts).sort(
+  (a, b) => new Date(b.date) - new Date(a.date)
+);
 
-console.log(sortedPosts.map(i => JSON.stringify(i, null, 2)))
-
-export const get = () => rss({
+export const get = () =>
+  rss({
     // `<title>` field in output xml
     title: `${SITE.name}`,
     // `<description>` field in output xml
@@ -17,14 +18,16 @@ export const get = () => rss({
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required frontmatter and advanced use cases
-    items: sortedPosts.map(item => ({
-        title: item.frontmatter.title,
-        description: item.frontmatter.description,
-        link: item.url,
-        pubDate: item.frontmatter.date,
-        image: `${SITE.url}${item.frontmatter.image}`,
+    items: sortedPosts.map((item) => ({
+      title: item.frontmatter.title,
+      description: item.frontmatter.description,
+      link: item.url,
+      pubDate: item.frontmatter.date,
+      customData: item.frontmatter.image
+        ? `<image>${SITE.url}${item.frontmatter.image}</image>`
+        : undefined,
     })),
     // (optional) inject custom xml
     customData: `<language>es-es</language>`,
-    stylesheet: '/rss/styles.xsl',
-});
+    stylesheet: "/rss/styles.xsl",
+  });
